14d9a7491ebb67c56c54b6afc0df99ae7b67927c
Jonas Paulsen <jonaspau@outlook.com>
2024-08-23 11:19:02 +0200

    Added points to make a pentagon

--- paths.py
+++ paths.py
@@ -5,7 +5,7 @@ window = pygame.display.set_mode((500, 400))
 
 while True:
     pygame.draw.lines(window, (255, 255, 255), True,
-                      ((50, 50), (75, 75), (25, 75)), 1)
+                      ((50, 50), (75, 75), (63, 100), (38, 100), (25, 75)), 1)
                      
     pygame.display.update()
 
