f1b7c70a80b954bf4ae2af7f7135ca74bbfac578
Jonas Paulsen <jonaspau@outlook.com>
2024-08-23 11:16:38 +0200

    Switched from draw.line to draw.lines to remove redundant code

--- paths.py
+++ paths.py
@@ -4,12 +4,8 @@ pygame.init()
 window = pygame.display.set_mode((500, 400))
 
 while True:
-    pygame.draw.line(window, (255, 255, 255),
-                     (50, 50), (75, 75), True)
-    pygame.draw.line(window, (255, 255, 255),
-                     (75, 75), (25, 75), True)
-    pygame.draw.line(window, (255, 255, 255),
-                     (25, 75), (50, 50), True)
-
+    pygame.draw.lines(window, (255, 255, 255), True,
+                      ((50, 50), (75, 75), (25, 75)), 1)
+                     
     pygame.display.update()
 
