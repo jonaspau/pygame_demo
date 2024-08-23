6c9d23716a765b97ebaa9da2a15cc6e793da53bb
Jonas Paulsen <jonaspau@outlook.com>
2024-08-23 10:35:06 +0200

    Added an ellipsis

--- hello.py
+++ hello.py
@@ -15,5 +15,8 @@ while True:
                      (40, 0, 50, 50))
     
     pygame.draw.circle(window, (255, 255, 0), (250, 200), 20, 10)
+
+    pygame.draw.ellipse(window, (255, 0, 0),
+                        (100, 100, 100, 50))
     
     pygame.display.update()
