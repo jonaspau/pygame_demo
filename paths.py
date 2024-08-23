2551ee1d220eed20110db244de0a2c7c9bcd5c28
Jonas Paulsen <jonaspau@outlook.com>
2024-08-23 11:06:12 +0200

    Added diagonal line

--- paths.py
+++ paths.py
@@ -4,5 +4,8 @@ pygame.init()
 window = pygame.display.set_mode((500, 400))
 
 while True:
+    pygame.draw.line(window, (255, 255, 255),
+                     (0, 0), (500, 400), 1)
+
     pygame.display.update()
 
